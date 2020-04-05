952a4d59eacdb27830d18ba93d55520433aea2aa
unknown <zkrozk>
2020-04-05 17:30:14 -0500

    Updates

---
+++ precache-manifest.3feb008c7215732093adc822114841f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d336e6a9e3edf19ccde",
+    "url": "/kuhniapilineas/static/css/main.6e54afc3.chunk.css"
+  },
+  {
+    "revision": "8d336e6a9e3edf19ccde",
+    "url": "/kuhniapilineas/static/js/main.8d336e6a.chunk.js"
+  },
+  {
+    "revision": "f9f13bfb8edf5e839538",
+    "url": "/kuhniapilineas/static/js/runtime~main.f9f13bfb.js"
+  },
+  {
+    "revision": "64cb1289f223837f2eea",
+    "url": "/kuhniapilineas/static/css/2.76038015.chunk.css"
+  },
+  {
+    "revision": "64cb1289f223837f2eea",
+    "url": "/kuhniapilineas/static/js/2.64cb1289.chunk.js"
+  },
+  {
+    "revision": "661f594d9d3c57ac89e08d72823b6e41",
+    "url": "/kuhniapilineas/static/media/logo-kuhni-strick-NEW.661f594d.webp"
+  },
+  {
+    "revision": "6be2e3643fc6dbf647bcf6cc2069b159",
+    "url": "/kuhniapilineas/static/media/machine.6be2e364.png"
+  },
+  {
+    "revision": "e9cd2f766ed718396bb9cac0f9034e37",
+    "url": "/kuhniapilineas/static/media/poligons.e9cd2f76.png"
+  },
+  {
+    "revision": "6d01fb9323a74d3f0663acc98e176858",
+    "url": "/kuhniapilineas/index.html"
+  }
+];
